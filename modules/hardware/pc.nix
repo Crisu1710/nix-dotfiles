569680e6e58db62be7c0021dbe72858bbe3684e5
robin <m2rs@posteo.de>
2026-07-19 18:38:19 +0200

    add PC

--- modules/hardware/pc.nix
+++ modules/hardware/pc.nix
@@ -29,7 +29,8 @@ _: {
             "sd_mod"
           ];
           kernelModules = [ ];
-          luks.devices."luks-c901bcac-a07e-4c6b-9be0-d0e24e0d19e6".device = "/dev/disk/by-uuid/c901bcac-a07e-4c6b-9be0-d0e24e0d19e6";
+          luks.devices."luks-c901bcac-a07e-4c6b-9be0-d0e24e0d19e6".device =
+            "/dev/disk/by-uuid/c901bcac-a07e-4c6b-9be0-d0e24e0d19e6";
         };
         kernelModules = [ "kvm-amd" ];
         extraModulePackages = [ ];
@@ -43,6 +44,10 @@ _: {
         "/mnt/games" = {
           device = "/dev/disk/by-uuid/4f96b204-be06-4ce4-8e4f-c38026f85da3";
           fsType = "ext4";
+          # options = [
+          #   "uid=1000"
+          #   "nofail"
+          # ];
         };
         "/boot" = {
           device = "/dev/disk/by-uuid/12D8-E80A";
--- modules/nixos/thinkpad.nix
+++ modules/nixos/thinkpad.nix
@@ -2,14 +2,14 @@ _: {
   flake.modules.nixos.thinkpad =
     { pkgs, ... }:
     {
-    # Aktiviert den python-validity Hintergrunddienst für die Firmware
-    services.python-validity.enable = true;
+      # Aktiviert den python-validity Hintergrunddienst für die Firmware
+      services.python-validity.enable = true;
 
-    # Aktiviert fprintd mit den Treibereinstellungen des Flakes
-    services.fprintd.enable = true;
+      # Aktiviert fprintd mit den Treibereinstellungen des Flakes
+      services.fprintd.enable = true;
 
-    # PAM-Authentifizierung für den Login und sudo aktivieren
-    security.pam.services.login.fprintAuth = true;
-    security.pam.services.sudo.fprintAuth = true;
+      # PAM-Authentifizierung für den Login und sudo aktivieren
+      security.pam.services.login.fprintAuth = true;
+      security.pam.services.sudo.fprintAuth = true;
     };
 }
